711dc19bcc05f06cb4fef9e263577bea451669fc
Jay Tang <jay.tang@edg.com.au>
2025-01-14 15:17:59 +1100

    Almost Sorted

---
+++ HackerRankApp.Tests/Problems/AlmostSortedTests.cs
@@ -0,0 +1,48 @@
+﻿using HackerRankApp.Problems;
+
+namespace HackerRankApp.Tests.Problems;
+
+public class AlmostSortedTests
+{
+	[Theory]
+	[ClassData(typeof(AlmostSortedTestData))]
+	public void Run_01(List<int> list, List<string> expectation)
+	{
+		var handleTask = () => AlmostSorted.Run(list);
+
+		handleTask.Should().NotThrow()
+			.Subject.Should().BeEquivalentTo(expectation, opts => opts.WithStrictOrdering());
+	}
+}
+
+public class AlmostSortedTestData : TheoryData<List<int>, List<string>>
+{
+	public AlmostSortedTestData()
+	{
+		Add([1, 2, 4, 9, 3], ["no"]);
+		Add([4, 2], ["yes", "swap 1 2"]);
+		Add([3, 1, 2], ["no"]);
+		Add([1, 5, 4, 3, 2, 6], ["yes", "reverse 2 5"]);
+		Add([1, 5, 4, 3, 2], ["yes", "reverse 2 5"]);
+		Add([1], ["yes"]);
+		Add([1, 2], ["yes"]);
+		Add([1, 2, 3], ["yes"]);
+		Add([1, 2, 4, 3], ["yes", "swap 3 4"]);
+		Add([1, 5, 3, 4, 2, 6], ["yes", "swap 2 5"]);
+		Add([1, 6, 3, 4, 5, 2], ["yes", "swap 2 6"]);
+
+		Add([1, 2, 3, 4, 5, 6], ["yes"]);
+		Add([6, 5, 4, 3, 2, 1], ["yes", "reverse 1 6"]);
+		Add([5, 4, 3, 2, 1, 6], ["yes", "reverse 1 5"]);
+		Add([1, 6, 5, 4, 3, 2], ["yes", "reverse 2 6"]);
+		Add([1, 5, 4, 3, 2, 6], ["yes", "reverse 2 5"]);
+
+		Add([5, 4, 3, 2, 1, 6, 7, 8], ["yes", "reverse 1 5"]);
+		Add([5, 4, 3, 2, 1, 6, 8, 7], ["no"]);
+		Add([8, 6, 4, 2, 9, 10, 11, 12], ["yes", "reverse 1 4"]);
+
+		Add([1, 2, 3, 9, 7, 8], ["no"]);
+		Add([1, 2, 3, 9, 7, 10], ["yes", "swap 4 5"]);
+		Add([1, 2, 3, 9, 7, 6], ["yes", "swap 4 6"]);
+	}
+}
---
+++ HackerRankApp/Problems/AlmostSorted.cs
@@ -0,0 +1,301 @@
+﻿namespace HackerRankApp.Problems;
+
+/// <summary>
+/// https://www.hackerrank.com/challenges/almost-sorted/problem?isFullScreen=true
+/// </summary>
+public static class AlmostSorted
+{
+	private const string Yes = "yes";
+	private const string No = "no";
+
+	private record NumberWindow(List<int> list)
+	{
+		public int PreviousIndex { get => CurrentIndex > 0 ? CurrentIndex - 1 : -1; }
+		public int CurrentIndex { get; set; } = -1;
+		public int StartIndex { get; set; } = -1;
+		public int EndIndex { get; set; } = -1;
+		public int Previous { get => PreviousIndex >= 0 ? list[PreviousIndex] : -1; }
+		public int Current { get => CurrentIndex >= 0 ? list[CurrentIndex] : -1; }
+		public int Start { get => StartIndex >= 0 ? list[StartIndex] : -1; }
+		public int End { get => EndIndex >= 0 ? list[EndIndex] : -1; }
+		public bool IsCurrentLast { get => CurrentIndex >= list.Count - 1; }
+		public int Size { get => StartIndex >= 0 && EndIndex >= 0 ? EndIndex - StartIndex + 1 : -1; }
+	}
+
+	private interface ISorter
+	{
+		public bool Actioned { get; }
+		public bool Disordered { get; }
+		List<string> GetResult();
+		void Process(int index);
+	}
+
+	private class Swapper(List<int> list) : ISorter
+	{
+		public bool Actioned { get => _actioned; }
+
+		public bool Disordered { get => _disordered; }
+
+		private readonly NumberWindow _window = new(list);
+
+		private bool _actioned = false;
+		private bool _disordered = false;
+
+		public void Process(int index)
+		{
+			if (_disordered) return;
+
+			_window.CurrentIndex = index;
+
+			if (_actioned)
+			{
+				if (IsDecreased())
+				{
+					_disordered = true;
+				}
+				return;
+			}
+
+			if (_window.StartIndex == -1)
+			{
+				_window.StartIndex = _window.CurrentIndex;
+				return;
+			}
+
+			if (_window.EndIndex == -1)
+			{
+				if (_window.Current >= _window.Start)
+				{
+					_window.StartIndex = _window.CurrentIndex;
+				}
+				else
+				{
+					_window.EndIndex = _window.CurrentIndex;
+
+					if (_window.IsCurrentLast)
+					{
+						_actioned = true;
+					}
+
+					if (_window.StartIndex > 0 && list[_window.StartIndex - 1] > _window.Current)
+					{
+						_disordered = true;
+					}
+				}
+				return;
+			}
+
+			// Start End is swappable
+			if (_window.Current >= _window.Start)
+			{
+				_actioned = true;
+
+				if (_window.Size > 2)
+				{
+					_disordered = true;
+				}
+			}
+			else if (_window.Current >= _window.End)
+			{
+				_window.EndIndex = _window.CurrentIndex;
+
+				if (_window.IsCurrentLast)
+				{
+					_disordered = true;
+				}
+			}
+			else if (_window.Current <= list[_window.StartIndex + 1])
+			{
+				_window.EndIndex = _window.CurrentIndex;
+				_actioned = true;
+			}
+			else
+			{
+				_disordered = true;
+			}
+		}
+
+		public List<string> GetResult()
+		{
+			if (_disordered)
+			{
+				return [No];
+			}
+
+			if (_actioned)
+			{
+				return [Yes, Swapped(_window.StartIndex + 1, _window.EndIndex + 1)];
+			}
+
+			return [Yes];
+		}
+
+		private bool IsDecreased()
+		{
+			// after the swap range
+			if (_window.CurrentIndex == _window.EndIndex + 1)
+			{
+				// smaller than the range
+				if (_window.Current < _window.Start)
+				{
+					return true;
+				}
+			}
+			// smaller than previous
+			else if (_window.Current < _window.Previous)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		private static string Swapped(int start, int end) => $"swap {start} {end}";
+	}
+
+	private class Reverser(List<int> list) : ISorter
+	{
+		public bool Actioned { get => _actioned; }
+
+		public bool Disordered { get => _disordered; }
+
+		private readonly NumberWindow _window = new(list);
+
+		private bool _actioned = false;
+		private bool _disordered = false;
+
+		public void Process(int index)
+		{
+			if (_disordered) return;
+
+			_window.CurrentIndex = index;
+
+			if (_actioned)
+			{
+				if (_window.CurrentIndex == _window.EndIndex + 1)
+				{
+					if (_window.Current < _window.Start)
+					{
+						_disordered = true;
+					}
+				}
+				else if (_window.Current < _window.Previous)
+				{
+					_disordered = true;
+				}
+				return;
+			}
+
+			if (_window.StartIndex == -1)
+			{
+				_window.StartIndex = index;
+				return;
+			}
+
+			if (_window.EndIndex == -1)
+			{
+				if (_window.Current >= _window.Start)
+				{
+					_window.StartIndex = _window.CurrentIndex;
+				}
+				else
+				{
+					_window.EndIndex = _window.CurrentIndex;
+
+					if (_window.IsCurrentLast)
+					{
+						_actioned = true;
+					}
+
+					if (_window.StartIndex > 0 && list[_window.StartIndex - 1] > _window.Current)
+					{
+						_disordered = true;
+					}
+				}
+				return;
+			}
+
+			if (_window.Current <= _window.Previous)
+			{
+				_window.EndIndex = _window.CurrentIndex;
+
+				if (_window.IsCurrentLast)
+				{
+					_actioned = true;
+				}
+
+				if (_window.StartIndex > 0 && list[_window.StartIndex - 1] > _window.Current)
+				{
+					_disordered = true;
+				}
+			}
+			else if (_window.Current >= _window.Start)
+			{
+				_actioned = true;
+			}
+			else
+			{
+				_disordered = true;
+			}
+
+
+			if (_window.Current >= _window.Start)
+			{
+				if (_window.EndIndex == -1)
+				{
+					_window.StartIndex = _window.CurrentIndex;
+				}
+				else
+				{
+					_actioned = true;
+				}
+
+				return;
+			}
+		}
+
+		public List<string> GetResult()
+		{
+			if (_disordered)
+			{
+				return [No];
+			}
+
+			if (_actioned)
+			{
+				return [Yes, Reversed(_window.StartIndex + 1, _window.EndIndex + 1)];
+			}
+
+			return [Yes];
+
+		}
+
+		private static string Reversed(int start, int end) => $"reverse {start} {end}";
+	}
+
+	public static List<string> Run(List<int> list)
+	{
+		if (list.Count == 0) return [No];
+
+		var sorters = new List<ISorter>()
+		{
+			new Swapper(list),
+			new Reverser(list)
+		};
+
+		for (int i = 0; i < list.Count; i++)
+		{
+			sorters.ForEach(e => e.Process(i));
+
+			if (sorters.All(e => e.Disordered)) break;
+		}
+
+		foreach (var sorter in sorters)
+		{
+			if (sorter.Disordered) continue;
+
+			return sorter.GetResult();
+		}
+
+		return sorters.First().GetResult();
+	}
+}
